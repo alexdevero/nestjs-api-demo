406acd8c770cf1143d8555cd556cc8193c197e91
Alex Devero <deveroalex@gmail.com>
2021-03-16 15:37:22 +0100

    Import books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common'
 
 import { CatsModule } from './cats/modules/cat.module'
 
+import { BooksModule } from './books/modules/books.module'
+
 @Module({
-  imports: [CatsModule]
+  imports: [BooksModule, CatsModule]
 })
 export class AppModule {}
