e99c3f4707821381f0d3b6152fbfac783af66bb5
Alex Devero <deveroalex@gmail.com>
2021-03-17 11:08:20 +0100

    Fix syntax

--- src/books/dto/book.dto.ts
+++ src/books/dto/book.dto.ts
@@ -1,9 +1,9 @@
 export class CreateBookDto {
-  id: string;
-  title: string;
-  author: string;
-  pubDate: string;
-  numOfPages: number;
+  id: string
+  title: string
+  author: string
+  pubDate: string
+  numOfPages: number
 }
 
 export class UpdateBookDto extends CreateBookDto {}
--- src/cats/dto/cat.dto.ts
+++ src/cats/dto/cat.dto.ts
@@ -1,5 +1,5 @@
 export class CreateCatDto {
-  id: string;
+  id: string
   name: string
   age: number
   breed: string
