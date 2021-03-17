616d9e599f2b0e031f588018a9533991605ea7a4
Alex Devero <deveroalex@gmail.com>
2021-03-17 14:49:43 +0100

    Import users API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 
 import { CatsModule } from './cats/modules/cat.module'
-
 import { BooksModule } from './books/modules/books.module'
+import { UsersModule } from './users/modules/users.module'
 
 @Module({
-  imports: [BooksModule, CatsModule]
+  imports: [BooksModule, CatsModule, UsersModule]
 })
 export class AppModule {}
